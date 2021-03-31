39f2c1b38f43a30aaf671487233890e8d74532e2
RoyalGuilds <Jared.trenholm12@gmail.com>
2021-03-31 11:23:45 -0300

    Initial commit

---
+++ Assets/ObjectInteraction.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    [Header("Interaction Type")]
+    public bool pickup;
+    public bool info;
+    public string message;
+
+    private Text infoText;
+
+    public bool dialogue;
+    [TextArea]
+    public string[] sentences;
+
+    public float DespawnTimer = 2f;
+    private float TimePassed = 0f;
+    private bool InfoUp = false;
+
+
+    private void Start()
+    {
+        infoText = GameObject.Find("InfoText").GetComponent<Text>();
+    }
+    public void Interaction()
+    {
+        if (pickup)
+        {
+            Pickup();
+        } else if (info)
+        {
+            Info();
+            TimePassed = 0f;
+        } else if (dialogue)
+        {
+            Dialogue();
+        }
+    }
+
+    private void Update()
+    {
+        if (InfoUp)
+        {
+            if(TimePassed >= DespawnTimer)
+            {
+                InfoUp = false;
+                infoText.text = null;
+            } else
+            {
+                TimePassed = TimePassed + Time.deltaTime;
+            }
+        }
+    }
+
+    private void Pickup()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    private void Info()
+    {
+        infoText.text = message;
+        InfoUp = true;
+    }
+
+    public void EndInfo()
+    {
+        InfoUp = false;
+        infoText.text = null;
+    }
+
+    private void Dialogue()
+    {
+        Debug.Log("Searching");
+        FindObjectOfType<DialogueManager>().StartDialogue(sentences);
+        Debug.Log("Finished");
+    }
+}
---
+++ Assets/PlayerInteraction.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    private GameObject InteractObject = null;
+
+    private ObjectInteraction InteractScript = null;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if(InteractObject != null)
+            {
+                InteractScript.Interaction();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Interactable")
+        {
+            InteractObject = other.gameObject;
+            InteractScript = other.gameObject.GetComponent<ObjectInteraction>();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(InteractObject.GetComponent<ObjectInteraction>().info == true)
+        {
+            InteractObject.GetComponent<ObjectInteraction>().EndInfo();
+        }
+        InteractObject = null;
+        InteractScript = null;
+    }
+}
